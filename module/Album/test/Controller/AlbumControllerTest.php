ae4135841cedd924b25c7c6cf2bca5d1ca341204
john@cemos.ph <john@cemos.ph>
2016-11-08 23:54:00 +0800

    adding test unit

--- module/Album/test/Controller/AlbumControllerTest.php
+++ module/Album/test/Controller/AlbumControllerTest.php
@@ -1,18 +1,22 @@
-<?
+<?php
+
 namespace AlbumTest\Controller;
 
-use Application\Controller\IndexController;
+use Album\Model\AlbumTable;
+use Album\Model\Album;
+use Album\Controller\AlbumController;
+use Prophecy\Argument;
+use Zend\ServiceManager\ServiceManager;
 use Zend\Stdlib\ArrayUtils;
 use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 
 class AlbumControllerTest extends AbstractHttpControllerTestCase
 {
+    protected $traceError = true;
+    protected $albumTable;
+
     public function setUp()
     {
-        // The module configuration should still be applicable for tests.
-        // You can override configuration here with test case specific values,
-        // such as sample view templates, path stacks, module_listener_options,
-        // etc.
         $configOverrides = [];
 
         $this->setApplicationConfig(ArrayUtils::merge(
@@ -21,27 +25,122 @@ class AlbumControllerTest extends AbstractHttpControllerTestCase
         ));
 
         parent::setUp();
+
+        $this->configureServiceManager($this->getApplicationServiceLocator());
+    }
+
+    protected function configureServiceManager(ServiceManager $services)
+    {
+        $services->setAllowOverride(true);
+
+        $services->setService('config', $this->updateConfig($services->get('config')));
+        $services->setService(AlbumTable::class, $this->mockAlbumTable()->reveal());
+
+        $services->setAllowOverride(false);
+    }
+
+    protected function updateConfig($config)
+    {
+        $config['db'] = [];
+        return $config;
+    }
+
+    protected function mockAlbumTable()
+    {
+        $this->albumTable = $this->prophesize(AlbumTable::class);
+        return $this->albumTable;
     }
 
     public function testIndexActionCanBeAccessed()
     {
-        $this->dispatch('/', 'GET');
+        $this->albumTable->fetchAll()->willReturn([]);
+
+        $this->dispatch('/album');
         $this->assertResponseStatusCode(200);
-        $this->assertModuleName('album');
-        $this->assertControllerName(AlbumController::class); // as specified in router's controller name alias
+        $this->assertModuleName('Album');
+        $this->assertControllerName(AlbumController::class);
         $this->assertControllerClass('AlbumController');
-        $this->assertMatchedRouteName('/album');
+        $this->assertMatchedRouteName('album');
     }
 
-    public function testIndexActionViewModelTemplateRenderedWithinLayout()
+    public function testAddActionCanBeAccessed()
     {
-        $this->dispatch('/', 'GET');
-        $this->assertQuery('.container .jumbotron');
+        $this->dispatch('/album/add');
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('Album');
+        $this->assertControllerName(AlbumController::class);
+        $this->assertControllerClass('AlbumController');
+        $this->assertMatchedRouteName('album');
     }
 
-    public function testInvalidRouteDoesNotCrash()
+    public function testEditActionCanBeAccessed($id=1)
     {
-        $this->dispatch('/invalid/route', 'GET');
-        $this->assertResponseStatusCode(404);
+        $this->albumTable->getAlbum($id)->willReturn(new Album());
+
+        $this->dispatch('/album/edit/'.$id);
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('Album');
+        $this->assertControllerName(AlbumController::class);
+        $this->assertControllerClass('AlbumController');
+        $this->assertMatchedRouteName('album');
     }
+
+    public function testDeleteActionCanBeAccessed($id=1)
+    {
+        $this->albumTable->getAlbum($id)->willReturn(new Album());
+        
+        $this->dispatch('/album/delete/'.$id);
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('Album');
+        $this->assertControllerName(AlbumController::class);
+        $this->assertControllerClass('AlbumController');
+    }
+
+
+    public function testAddActionRedirectsAfterValidPost()
+    {
+        $this->albumTable
+            ->saveAlbum(Argument::type(Album::class))
+            ->shouldBeCalled();
+
+        $postData = [
+            'title'  => 'Led Zeppelin III',
+            'artist' => 'Led Zeppelin',
+            'id'     => '',
+        ];
+        $this->dispatch('/album/add', 'POST', $postData);
+        $this->assertResponseStatusCode(302);
+        $this->assertRedirectTo('/album');
+    }
+
+    public function testEditActionRedirectsAfterValidPost($id=1)
+    {
+        $this->albumTable
+            ->saveAlbum(Argument::type(Album::class))
+            ->shouldBeCalled();
+
+        $this->albumTable->getAlbum($id)->willReturn(new Album());
+
+        $postData = [
+            'title'  => 'Led Zeppelin III',
+            'artist' => 'Led Zeppelin',
+            'id'     => $id,
+        ];
+        $this->dispatch('/album/edit/'.$id, 'POST', $postData);
+        $this->assertResponseStatusCode(302);
+        $this->assertRedirectTo('/album');
+    }
+
+    public function testDeleteActionRedirectsAfterValidPost($id=1)
+    {
+        $this->albumTable->getAlbum($id)->willReturn(new Album());
+
+        $postData = [
+            'id'     => $id,
+        ];
+        $this->dispatch('/album/delete/'.$id, 'POST', $postData);
+        $this->assertResponseStatusCode(302);
+        $this->assertRedirectTo('/album');
+    }
+
 }
