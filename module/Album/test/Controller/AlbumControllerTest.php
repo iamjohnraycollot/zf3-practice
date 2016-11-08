8e23e2d388922301afea3be33ccdcc3c1ba4f2c5
john@cemos.ph <john@cemos.ph>
2016-11-08 17:43:34 +0800

    added phpunit test for album controller

---
+++ module/Album/test/Controller/AlbumControllerTest.php
@@ -0,0 +1,47 @@
+<?
+namespace AlbumTest\Controller;
+
+use Application\Controller\IndexController;
+use Zend\Stdlib\ArrayUtils;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class AlbumControllerTest extends AbstractHttpControllerTestCase
+{
+    public function setUp()
+    {
+        // The module configuration should still be applicable for tests.
+        // You can override configuration here with test case specific values,
+        // such as sample view templates, path stacks, module_listener_options,
+        // etc.
+        $configOverrides = [];
+
+        $this->setApplicationConfig(ArrayUtils::merge(
+            include __DIR__ . '/../../../../config/application.config.php',
+            $configOverrides
+        ));
+
+        parent::setUp();
+    }
+
+    public function testIndexActionCanBeAccessed()
+    {
+        $this->dispatch('/', 'GET');
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('album');
+        $this->assertControllerName(AlbumController::class); // as specified in router's controller name alias
+        $this->assertControllerClass('AlbumController');
+        $this->assertMatchedRouteName('/album');
+    }
+
+    public function testIndexActionViewModelTemplateRenderedWithinLayout()
+    {
+        $this->dispatch('/', 'GET');
+        $this->assertQuery('.container .jumbotron');
+    }
+
+    public function testInvalidRouteDoesNotCrash()
+    {
+        $this->dispatch('/invalid/route', 'GET');
+        $this->assertResponseStatusCode(404);
+    }
+}
